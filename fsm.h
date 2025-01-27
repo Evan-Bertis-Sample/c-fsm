d84a32b4dd197ce50f5950d83b99fabd36d0539f
evan-bertis-sample <esample21@gmail.com>
2025-01-26 18:49:51 -0600

    Change structures to be a good bit smarter with who owns what memory and whatnot

--- fsm.h
+++ fsm.h
@@ -14,7 +14,10 @@
 extern "C" {
 #endif  // __cplusplus
 
+#include <stdbool.h>
 #include <stdint.h>
+
+#define FSM_DEBUG 1
 
 struct fsm;
 typedef void (*fsm_state_fn)(struct fsm *fsm, void *context);
@@ -28,17 +31,10 @@ typedef void (*fsm_dealloc_fn)(void *ptr);
 /// @brief Describes a state in the FSM
 typedef struct fsm_state {
     char *name;
-    void *context;
-
     fsm_state_fn on_enter;
     fsm_state_fn on_exit;
 } fsm_state_t;
 
-fsm_state_t *fsm_state_create(fsm_alloc_fn alloc_fn, char *name, void *context, fsm_state_fn on_enter, fsm_state_fn on_exit);
-void fsm_state_destroy(fsm_dealloc_fn dealloc_fn, fsm_state_t *state);
-
-#define FSM_STATE_CREATE(...) fsm_state_create(malloc, __VA_ARGS__)
-#define FSM_STATE_DESTROY(state) fsm_state_destroy(free, state)
 
 /// @brief Describes a transition in the FSM
 typedef struct fsm_predicate_group {
@@ -46,46 +42,78 @@ typedef struct fsm_predicate_group {
     fsm_size_t predicate_count;
 } fsm_predicate_group_t;
 
-fsm_predicate_group_t *fsm_predicate_group_create(fsm_alloc_fn alloc_fn, fsm_transition_predicate_fn *predicates, fsm_size_t predicate_count);
-void fsm_predicate_group_destroy(fsm_dealloc_fn dealloc_fn, fsm_predicate_group_t *group);
-
-#define FSM_PREDICATE_GROUP_CREATE(...) fsm_predicate_group_create(malloc, __VA_ARGS__, sizeof(__VA_ARGS__) / sizeof(fsm_transition_predicate_fn))
-#define FSM_PREDICATE_GROUP_DESTROY(group) fsm_predicate_group_destroy(free, group)
-
 /// @brief Describes a transition in the FSM
 /// @note This is an internal structure used to store transitions
 ///      in the FSM, do not use this directly
 typedef struct __fsm_transition {
-    fsm_state *from;
-    fsm_state *to;
+    fsm_state_t *from;
+    fsm_state_t *to;
     fsm_predicate_group_t *predicates;
 } __fsm_transition_t;
 
 typedef struct fsm {
-    fsm_state *states;
-    fsm_size_t __state_count;
+    void *context;
 
+    /// @brief Array of states in the FSM -- strong reference, we own this memory
+    fsm_state_t *states;
+    /// @brief Array of transitions in the FSM -- strong reference, we own this memory
     __fsm_transition_t *transitions;
+
+    fsm_alloc_fn __alloc_fn;
+    fsm_dealloc_fn __dealloc_fn;
+
+    size_t __context_size;
+    fsm_size_t __state_count;
     fsm_size_t __transition_count;
-
     fsm_size_t __current_state_idx;
-
-    bool is_running;
+    bool __is_running;
 } fsm_t;
 
-void fsm_init(fsm_t *fsm, fsm_state *states, fsm_size_t state_count, fsm_predicate_group_t *transitions, fsm_size_t transition_count);
+fsm_t *fsm_create(fsm_alloc_fn alloc_fn, fsm_dealloc_fn dealloc_fn);
 void fsm_run(fsm_t *fsm);
 void fsm_stop(fsm_t *fsm);
+void fsm_destroy(fsm_t *fsm);
 
-void fsm_add_state(fsm_t *fsm, fsm_state *state);
-void fsm_add_transition(fsm_t *fsm, fsm_predicate_group_t *transition);
-void fsm_add_transition(fsm_t *fsm, char *from, char *to, fsm_transition_predicate_fn *predicates, fsm_size_t predicate_count);
+/* 
+* Note about the fsm_add_xxx functions:
+* Whenever you add a state or transition to the FSM, the FSM will take ownership of the memory
+* and will free it when the FSM is destroyed. This means that you should not free the memory.
+* Modifying whatever fsm_state_t or fsm_predicate_group_t you pass to the fsm_add_xxx functions
+* after you pass them to the FSM will not have any effect on the FSM.
+* Instead, a copy of the data will be made and stored in the FSM.
+*/ 
+
+void fsm_add_state(fsm_t *fsm, fsm_state_t state);
+void fsm_add_transition(fsm_t *fsm, char *from, char *to, fsm_predicate_group_t predicates);
+void fsm_add_transition_from_all(fsm_t *fsm, char *to, fsm_predicate_group_t predicates);
+void fsm_add_transition_to_all(fsm_t *fsm, char *from, fsm_predicate_group_t predicates);
 
 inline fsm_size_t fsm_state_count(fsm_t *fsm) { return fsm->__state_count; }
 inline fsm_size_t fsm_transition_count(fsm_t *fsm) { return fsm->__transition_count; }
+inline char *fsm_current_state(fsm_t *fsm) { return fsm->states[fsm->__current_state_idx].name; }
+inline bool fsm_is_running(fsm_t *fsm) { return fsm->__is_running; }
+
 
 fsm_size_t __fsm_state_index(fsm_t *fsm, char *name);
 fsm_size_t __fsm_transition_index(fsm_t *fsm, char *from, char *to);
+
+#define FSM_CREATE() fsm_create(malloc, free)
+
+#define FSM_LINE __LINE__
+#define FSM_STR_HELPER(x) #x
+#define FSM_STR(x) FSM_STR_HELPER(x)
+
+#define FSM_ERROR_COLOR "\033[0;31m"
+#define FSM_RESET_COLOR "\033[0m"
+
+#ifdef FSM_DEBUG
+#define FSM_DEUBG_PREFIX "[fsm.h:" FSM_STR(FSM_LINE) "] "
+#define FSM_LOG(fmt, ...) printf(FSM_DEUBG_PREFIX fmt, ##__VA_ARGS__)
+#define FSM_LOG_ERROR(fmt, ...) fprintf(stderr, FSM_ERROR_COLOR FSM_DEUBG_PREFIX FSM_RESET_COLOR fmt, ##__VA_ARGS__)
+#else
+#define FSM_LOG(fmt, ...) void(0)
+#define FSM_LOG_ERROR(fmt, ...) void(0)
+#endif  // FSM_DEBUG
 
 #ifdef FSM_IMPL
 
